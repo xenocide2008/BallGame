5a73221f28713219607deee04eda55262d062ef2
xenocide2008 <84458114+xenocide2008@users.noreply.github.com>
2021-07-30 20:15:57 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,35 @@
+var ball;
+var database,position;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database.ref('ball/position').on("value",function(data){
+        position=data.val()
+        ball.x=position.x
+        ball.y=position.y
+    })
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+  database.ref('ball/position').update({x:position.x+x,y:position.y+y})
+}
+
